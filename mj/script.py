2793359015b8be7d0a0a5e78b30b37c910bb2f3a
mjshooshtari <mjavadshooshtari@gmail.com>
2024-03-01 15:23:26 -0500

    created mj moved and modified  script.py

--- mj/script.py
+++ mj/script.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello, my name is [Name].")
+    print("Hello, my name is mj.")
     print("I am participating in the graduate seminar class.")
     # Feel free can add more lines here if you want to share more.
 
